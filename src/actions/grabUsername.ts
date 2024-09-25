88b919c5b72eee32d38150eb4f55408fa9c3d8df
Alif Hakimi <alifhakimiazwan@gmail.com>
2024-09-26 03:37:53 +0800

    formData

--- src/actions/grabUsername.ts
+++ src/actions/grabUsername.ts
@@ -3,18 +3,20 @@ import { authOptions } from "@/app/api/auth/[...nextauth]";
 import { Page } from "@/models/Page";
 import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
-const grabUsername = async (formData) => {
-  const username = formData.get("username");
-  mongoose.connect(process.env.MONGODB_URI);
+
+const grabUsername = async (formData: FormData) => {
+  const username = formData.get("username") as string; // Ensure username is a string
+  await mongoose.connect(process.env.MONGODB_URI);
+
   const existingPageDoc = await Page.findOne({ uri: username });
   if (existingPageDoc) {
-    return false;
+    return false; // Username is taken
   } else {
     const session = await getServerSession(authOptions);
 
     if (session) {
       await Page.create({ uri: username, owner: session?.user?.email });
-      return true;
+      return true; // Username available and created
     }
   }
 };
